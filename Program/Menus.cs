5e79928c2db123f5a6f2c42f41dedf0a8277b6a5
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-06 08:01:38 -0700

    Agregar archivos de proyecto.

---
+++ Program/Menus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Menus
+    {
+        public string Identificador { get; set; }
+        public int HoraInicio { get; set; }
+        public int HoraFin { get; set; }
+
+        public string Nombre { get; set; }
+
+        public Menus()
+        {
+            new List<Platillos>();
+        }
+
+        public Menus(string nombre)
+        {
+            Nombre = nombre;
+            Nombre = "Nuevo Menu";
+        }
+    }
+}
---
+++ Program/Platillos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Platillos
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public float Precio { get; set; }
+
+        public Platillos()
+        {
+            new List<Ingredientes>();
+        }
+
+        public Platillos(float precio)
+        {
+            Precio = precio;
+            if (precio <= 50)
+            {
+                Console.WriteLine("El platillo no se puede vender");
+            } else
+            {
+                Console.WriteLine("El precio del platillo es: " + precio);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
